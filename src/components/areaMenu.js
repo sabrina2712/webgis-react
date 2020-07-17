a49976812007f0ecc115b755257b4cd264458cc2
sabrina2712 <mailroshni@gmail.com>
2020-07-17 16:35:56 +0200

    added menu component

--- src/components/areaMenu.js
+++ src/components/areaMenu.js
@@ -6,12 +6,25 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Fade from '@material-ui/core/Fade';
 import { render } from '@testing-library/react';
 import { fromLonLat, get } from "ol/proj"
+import Map from 'ol/Map';
+import View from 'ol/View';
+import GeoJSON from 'ol/format/GeoJSON';
 
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { OSM, Vector as VectorSource } from 'ol/source';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
 
-export default function FadeMenu() {
+export default function FadeMenu(props) {
     const [anchorEl, setAnchorEl] = React.useState(null);
-    const [locations,setLocations] = React.useState({"TAR" : ([34.767511,36.842215 ]),
-    "Rome" : ([ 12.496366, 41.902782])})
+    const [locations,setLocations] = React.useState({"TAR" : [34.767511,36.842215 ],
+    "Rome" : [ 12.496366, 41.902782]});
+
+   
+
     
     const open = Boolean(anchorEl);
   
@@ -22,17 +35,21 @@ export default function FadeMenu() {
     const handleClose = () => {
       setAnchorEl(null);
     };
-
+   const map = props.map
+    
 
   const goToLocation =(evt)=>{
+ 
     let selectedArea = evt.target.name;
-        let coor =this.state.locations[evt.target.name];
+  
+        let coor =locations[evt.target.name];
         console.log(selectedArea)
-        let view =this.state.map.getView()
-        this.toggleLocation()
+        let view = map.getView()
+        console.log( map)
         
-    if (evt.target.checked === true){
-        let view =  this.state.map.getView()
+    if (evt.target === true){
+        const map = props.map
+        let view =  map.getView()
             view.animate({
                 center:fromLonLat(coor),
                 zoom: 11,
@@ -47,6 +64,8 @@ export default function FadeMenu() {
             }); 
 
     }}
+
+
 
  
     return (
@@ -64,18 +83,32 @@ export default function FadeMenu() {
                 onClose={handleClose}
                 TransitionComponent={Fade}
             >
-            <MenuItem onClick={handleClose}
-               
-                name ="TAR"
-                onChange={goToLocation}>TAR
+            <MenuItem onClick={handleClose}>
+           
+                            <ListItemIcon>
+                                <Checkbox 
+                                id= "cb1"
+                                color="primary"
+                                name ="TAR"
+                                onClick={goToLocation}
+                                />
+                            </ListItemIcon>
+                            <ListItemText primary="TAR" />
+                      
                 
             </MenuItem>
-            <MenuItem onClick={handleClose}
-              
-                name ="Rome"
-                onChange={goToLocation}>Rome
-            </MenuItem>
-                
+            <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                                <Checkbox 
+                               
+                                id= "cb2"
+                                    color="primary"
+                                    name ="Rome"
+                                    onClick={goToLocation}
+                                />
+                            </ListItemIcon>
+                            <ListItemText primary="Rome" />
+                            </MenuItem>
         </Menu>
         </div>
   );
